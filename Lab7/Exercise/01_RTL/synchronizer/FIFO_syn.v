79fafdb079e4f2715b93a4e38f16e2423ff0d159
timchen1015 <tim.chen60101@gmail.com>
2026-06-25 16:25:39 +0800

    FEAT: CDC handshake and AFIFO method

--- Lab7/Exercise/01_RTL/synchronizer/FIFO_syn.v
+++ Lab7/Exercise/01_RTL/synchronizer/FIFO_syn.v
@@ -1,17 +1,121 @@
-module FIFO_syn #(parameter WIDTH=8, parameter WORDS=64) (
-    input wclk,
-    input rclk,
-    input rst_n,
-    input winc,
-    input [WIDTH-1:0] wdata,
-    output reg wfull,
-    input rinc,
-    output reg [WIDTH-1:0] rdata,
-    output reg rempty,
-    input  flag_clk1_to_fifo,
-    input  flag_clk2_to_fifo,
-    output flag_fifo_to_clk1,
-    output flag_fifo_to_clk2
+module FIFO_syn #(
+    parameter WIDTH = 8,
+    parameter WORDS = 64
+)(
+    input  wire             wclk,
+    input  wire             rclk,
+    input  wire             rst_n,
+
+    // write domain
+    input  wire             winc,      // write request
+    input  wire [WIDTH-1:0] wdata,
+    output reg              wfull,
+
+    // read domain
+    input  wire             rinc,      // read request
+    output reg  [WIDTH-1:0] rdata,
+    output reg              rempty
 );
+
+// --------------------------------------------------------
+localparam ADDR_WIDTH = $clog2(WORDS);
+reg [WIDTH-1:0] mem [0:WORDS-1];
+
+// write pointer(All pointer got extra 1 bit to distinguish between full and empty)
+reg [ADDR_WIDTH:0] wptr_bin;
+reg [ADDR_WIDTH:0] wptr_bin_next;
+reg [ADDR_WIDTH:0] wptr_gray;
+reg [ADDR_WIDTH:0] wptr_gray_next;
+
+
+// read pointer
+reg [ADDR_WIDTH:0] rptr_bin;
+reg [ADDR_WIDTH:0] rptr_bin_next;
+reg [ADDR_WIDTH:0] rptr_gray;
+reg [ADDR_WIDTH:0] rptr_gray_next;
+
+
+// synchronized pointers
+wire [ADDR_WIDTH:0] wptr_gray_sync;
+wire [ADDR_WIDTH:0] rptr_gray_sync;
+
+
+NDFF_syn #(.WIDTH(ADDR_WIDTH+1)) u_wptr_sync (
+    .D(wptr_gray),
+    .Q(wptr_gray_sync),
+    .clk(rclk),
+    .rst_n(rst_n)
+);
+
+NDFF_syn #(.WIDTH(ADDR_WIDTH+1)) u_rptr_sync (
+    .D(rptr_gray),
+    .Q(rptr_gray_sync),
+    .clk(wclk),
+    .rst_n(rst_n)
+);
+
+wire rempty_next = (rptr_gray_next == wptr_gray_sync);
+
+// For full detection, invert the two MSBs of the synchronized read pointer and keep the remaining lower bits unchanged
+wire wfull_next = (wptr_gray_next == {
+    ~rptr_gray_sync[ADDR_WIDTH:ADDR_WIDTH-1],
+     rptr_gray_sync[ADDR_WIDTH-2:0]
+});
+
+wire r_en = rinc && !rempty;
+wire w_en = winc && !wfull;
+
+
+// Write Domain logic
+always @(posedge wclk or negedge rst_n) begin
+    if (!rst_n) begin
+        wptr_bin  <= 0;
+        wptr_gray <= 0;
+        wfull     <= 1'b0;
+    end else begin
+        wptr_bin  <= wptr_bin_next;
+        wptr_gray <= wptr_gray_next;
+        wfull     <= wfull_next;
+    end
+end
+
+// Memory write
+always @(posedge wclk) begin
+    if (w_en) begin
+        mem[wptr_bin[ADDR_WIDTH-1:0]] <= wdata;
+    end
+end
+
+
+// Read Domain logic
+always @(posedge rclk or negedge rst_n) begin
+    if (!rst_n) begin
+        rptr_bin  <= 0;
+        rptr_gray <= 0;
+        rempty    <= 1'b1;
+    end else begin
+        rptr_bin  <= rptr_bin_next;
+        rptr_gray <= rptr_gray_next;
+        rempty    <= rempty_next;
+    end
+end
+
+// Memory read
+always @(posedge rclk or negedge rst_n) begin
+    if (!rst_n) begin
+        rdata <= {WIDTH{1'b0}};
+    end else if (r_en) begin
+        rdata <= mem[rptr_bin[ADDR_WIDTH-1:0]];
+    end
+end
+
+
+// Calculate next pointers and gray codes
+always @(*)begin
+    wptr_bin_next = w_en ? wptr_bin + 1'b1 : wptr_bin;
+    wptr_gray_next = (wptr_bin_next >> 1) ^ wptr_bin_next;
+    rptr_bin_next = r_en ? rptr_bin + 1'b1 : rptr_bin;
+    rptr_gray_next = (rptr_bin_next >> 1) ^ rptr_bin_next;
+end
 
 endmodule
--- Lab7/Exercise/01_RTL/synchronizer/Handshake_syn.v
+++ Lab7/Exercise/01_RTL/synchronizer/Handshake_syn.v
@@ -1,17 +1,89 @@
-module Handshake_syn #(parameter WIDTH=8) (
-    input sclk,
-    input dclk,
-    input rst_n,
-    input sready,
-    input [WIDTH-1:0] din,
-    input dbusy,
-    output sidle,
-    output reg dvalid,
-    output reg [WIDTH-1:0] dout,
-    input  flag_clk1_to_handshake,
-    input  flag_clk2_to_handshake,
-    output flag_handshake_to_clk1,
-    output flag_handshake_to_clk2
+module Handshake_syn #(
+    parameter WIDTH = 8
+)(
+    input  wire             sclk,
+    input  wire             dclk,
+    input  wire             rst_n,
+
+    // source domain
+    input  wire             svalid,   // source has valid data
+    input  wire [WIDTH-1:0] din,
+    output wire             sready,   // handshake module can accept data
+
+    // destination domain
+    output reg              dvalid,   // destination data valid
+    output reg  [WIDTH-1:0] dout,
+    input  wire             dready    // destination can accept data
 );
 
+/*
+Handshake flow:
+1. The source asserts svalid when it has valid data.
+2. If the handshake module is idle, it asserts sready.
+3. When svalid && sready is true, the source data is latched.
+4. A request is sent to the destination clock domain.
+5. After the destination receives the request, it samples the latched data into dout.
+6. The destination asserts dvalid to indicate that dout is valid.
+7. When dvalid && dready is true, the destination has accepted the data.
+8. An acknowledge signal is sent back to the source clock domain.
+9. After the source receives the acknowledge, it can send the next data.
+*/
+
+// Use toggle based handshake signals to cross clock domains
+reg sreq;                //request toggles while send data
+wire dreq;               // module output use wire only
+reg dack;               // acknowledge toggles while receive data
+wire sack;               // module output use wire only
+
+
+reg [WIDTH-1:0] dhold;    // hold data to send to destination
+
+NDFF_syn u_req (
+    .D(sreq),
+    .clk(dclk),
+    .rst_n(rst_n),
+    .Q(dreq)
+);
+
+NDFF_syn u_ack (
+    .D(dack),
+    .clk(sclk),
+    .rst_n(rst_n),
+    .Q(sack)
+);
+
+assign sready = (sack == sreq);
+
+
+//----------Source Clock Domain Logic----------
+always @(posedge sclk or negedge rst_n) begin
+    if (!rst_n) begin
+        dhold <= 0;
+        sreq <= 0;
+    end else if (svalid && sready) begin
+        dhold <= din;
+        sreq <= ~sreq;                      // toggle request when data is sent
+    end
+end
+
+
+
+// ---------Destination Clock Domain Logic----------
+
+// New request arrive → latch dhold into dout → assert dvalid
+// Destination accept data → deassert dvalid -> toggle dack
+
+always @(posedge dclk or negedge rst_n) begin
+    if (!rst_n) begin
+        dout <= 0;
+        dvalid <= 0;
+        dack <= 0;
+    end else if (!dvalid && (dreq != dack)) begin         // new request arrive
+        dout <= dhold;                                    // latch data
+        dvalid <= 1;                                      // assert dvalid
+    end else if (dvalid && dready) begin
+        dvalid <= 0;                        // deassert dvalid when destination accepts data
+        dack <= dreq;                      // Acknowledge the request
+    end
+end
 endmodule
